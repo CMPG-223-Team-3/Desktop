1c584e481c27178d792b44eb46465e78798321e4
Dean Minnie <dminnie33@gmail.com>
2020-10-08 15:48:34 +0200

    Removed borders
    Removed all the borders form the chiled forms.

--- Desktop/Database.Designer.cs
+++ Desktop/Database.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Database
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Database";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Database";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
