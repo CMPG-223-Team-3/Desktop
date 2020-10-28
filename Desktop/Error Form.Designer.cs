208a7b08eac4115809b5b640ea15902289d813f0
Dean Minnie <dminnie33@gmail.com>
2020-10-28 14:51:25 +0200

    Error box

---
+++ Desktop/Error Form.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Desktop
+{
+    partial class Error_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TheText = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TheText
+            // 
+            this.TheText.AutoSize = true;
+            this.TheText.Location = new System.Drawing.Point(12, 9);
+            this.TheText.Name = "TheText";
+            this.TheText.Size = new System.Drawing.Size(35, 13);
+            this.TheText.TabIndex = 0;
+            this.TheText.Text = "label1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(466, 155);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Error_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 190);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.TheText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Error_Form";
+            this.Text = "Error_Form";
+            this.Load += new System.EventHandler(this.Error_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TheText;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
